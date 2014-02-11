84bcc5f5973b10d0cc5d278835d18c4173129a8a
Z. D. Smith <cruxdestruct@gmail.com>
2014-02-11 16:31:57 -0500

    Things are currently working. We are splitting up strings and working on them separately.

--- parser.py
+++ parser.py
@@ -26,9 +26,58 @@ class Body(object):
 	def append(self, glyph):
 		self.glyph_list.append(glyph)
 
-def lookup_and_substitute(word, set):
-	# we need to split our list into substrings and work on those.
+def word_manager(word, func):
+	substring = []
+	subword = False
+	for position in word:
+		# souRCE
+		if isinstance(position, str) and not subword:
+			sublist.append(position)
+		# CHapter
+		elif not isinstance(position, str) and not sublist:
+			continue
+		elif not instance(position, str) and sublist:
+			subword = str(sublist)
+			return lookup_and_substitute(word, set)
+
+def separate_strings_and_symbols(token_list):
+	"""
 	
+	>>> separate_strings_and_symbols(['a', 'b', Glyph('RCE'), 'h'])
+	(['ab', 'h'], [RCE])
+	
+	"""
+	
+	
+	symbol_indices_and_symbols = [(i,x) for i,x in enumerate(token_list) if isinstance(x, Glyph)]
+	symbol_indices, symbols = (zip(*symbol_indices_and_symbols)
+							   if symbol_indices_and_symbols else ((),()))
+
+	string_indices = [(i+1, j) for i,j in zip((-1,)+symbol_indices,symbol_indices+(len(token_list),))]
+	string_list = ["".join(token_list[i:j]) for i,j in string_indices]
+	return (string_list, list(symbols))
+
+def for_each_string(list, func):
+	"""
+	Takes a list of chars and non-chars, makes strings out of 
+	consecutive chars, and then applies a function to those chars.
+	Returns a list of chars and non-chars.
+	"""
+	strings, symbols = separate_strings_and_symbols(list)
+	new_strings = [func(string) for string in strings]
+	
+	product = new_strings[0][:]
+	for symbol, string in zip(symbols, strings[1:]):
+		product.append(symbol)
+		product.extend(string)
+	return product	
+
+def substitute_trigraphs(word): 
+	return lookup_and_substitute(word, ABB_SET.TRIGRAPHS)
+def substitute_digraphs(word):
+	return lookup_and_substitute(word, ABB_SET.DIGRAPHS)
+	
+def lookup_and_substitute(word, set):
 	for master, value in set.iteritems():
 		pattern = re.compile(master)
 		matches = pattern.finditer(word)
@@ -49,16 +98,21 @@ def abbreviate(word):
 	None
 	
 	>>> abbreviate("source")
+	['s', 'o', 'u', RCE]
 	"""
 	# return a full-word substitute
-	if word in ABB_SET.WORDS:
-		return Glyph(ABB_SET.WORDS[word])
+	# TODO: Short-circuit full words
+	#if word in ABB_SET.WORDS:
+	#	return Glyph(ABB_SET.WORDS[word])
 	
 	else:
-		
-		word = lookup_and_substitute(word, ABB_SET.TRIGRAPHS)
-		#word = lookup_and_substitute(word, ABB_SET.DIGRAPHS)
-		
+		# We could put this into a loop that goes through dictionary sets
+		result = for_each_string(list(word), 
+								lambda x: lookup_and_substitute(x, ABB_SET.WORDS))	
+		result = for_each_string(list(word), 
+								lambda x: lookup_and_substitute(x, ABB_SET.TRIGRAPHS))
+		result = for_each_string(list(word), 
+								lambda x: lookup_and_substitute(x, ABB_SET.DIGRAPHS))		
 	return word
 		
 def parse(text):
@@ -70,3 +124,5 @@ def parse(text):
 if __name__ == '__main__':
 	import doctest
 	doctest.testmod()
+	
+	#abbreviate("source")
