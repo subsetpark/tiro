edc78eab4da0101bd8ab44e65d345e160debd34d
Z. D. Smith <cruxdestruct@gmail.com>
2014-02-18 11:36:26 -0500

    More docstrings, a couple bug fixes

--- abba.py
+++ abba.py
@@ -31,7 +31,7 @@ class Abbreviation_dictionary(object):
 	
 	"""
 	def __init__(self, config):
-		self.abb_sequences = [[],[],[],[],[]]
+		self.abb_sequences = []
 		self.lookup_table = {}
 		# begin creating unicode characters at the beginning 
 		# of the private use space
@@ -46,12 +46,16 @@ class Abbreviation_dictionary(object):
 				# It's a representation method. Add it to the lookup.
 				if re.search(rep_search, option):
 					has_a_rep = True
-					self.add_to_lookup(codepoint, section, option=option, value=self.parse_rules(list(config[section][option])))
+					self.add_to_lookup(codepoint, section, option=option, 
+						value=self.parse_rules(list(config[section][option])))
 			if not has_a_rep: 
 				self.add_to_lookup(codepoint, section)						
-			# find the list corresponding to the regnet's prec, create an abbreviation with info from the regnet, add the abbreviation to the list, then add the uni_rep to the lookup table.
 	
 	def add_to_lookup(self, codepoint, section, option=None, value=None):
+		"""
+		Add a representation method to the reverse lookup table as we create
+		the dictionary.
+		"""
 		if not codepoint in self.lookup_table:
 			self.lookup_table[codepoint] = { 'name': section, option: value}
 		self.lookup_table[codepoint][option] = value
@@ -60,22 +64,33 @@ class Abbreviation_dictionary(object):
 		return self.lookup_table[char]['name']
 		
 	def uni_lookup(self, char):
+		"""
+		Return unicode representation of abbreviation
+		"""
 		if 'uni_rep' in self.lookup_table[char]:
 			return self.lookup_table[char]['uni_rep']
 		else:
 			return self.lookup_table[char]['name']
 			
 	def add_to_dict(self, section, regnet, serial):
-		if not self.abb_sequences[regnet.prec]:
-			self.abb_sequences[regnet.prec] = [Abbreviation(
-								section, regnet.pattern, serial)]
-		else:
-			self.abb_sequences[regnet.prec].append(Abbreviation(
+		"""
+		Given the makings of an abbreviation, create a new object
+		and add it to the sequences list.
+		"""
+		# build abb_sequences to the number of prec levels
+		while len(self.abb_sequences) < regnet.prec + 1:
+			self.abb_sequences.append([])
+		# add a new abbreviation object to the correct prec sequence
+		self.abb_sequences[regnet.prec].append(Abbreviation(
 								section, regnet.pattern, serial))
 								
 	def parse_rules(self, value):
-		# Recurse through the uni_rep, replacing codepoint references as you go.
-		if '{' not in value:
+		"""
+		Read the formatted .ini looking for references to unicode characters. 
+		Recurse through the uni_rep, replacing codepoint references as you go.
+		"""
+		
+		if '{' not in value or value[value.index('{')+5] is not '}':
 			return ''.join(value)
 		else:
 			i = value.index('{')
@@ -88,17 +103,15 @@ class Abbreviation_dictionary(object):
 		Each transform subs in a dynamically generated unicode 
 		control character.
 		"""
-		working_word = text
 		for abbreviation in sequence:
-			working_word = re.sub(abbreviation.pattern, abbreviation.codepoint, working_word) 
-		return working_word	
+			text = re.sub(abbreviation.pattern, abbreviation.codepoint, text) 
+		return text	
 	
 	def abbreviate_text(self, text):
 		"""
 		Runs each sequence of transforms in the order they were loaded into the
 		controller.
 		"""
-
 		working_text = text
 		for sequence in self.abb_sequences:
 			working_text = self.lookup_and_substitute(working_text, sequence)
@@ -107,20 +120,27 @@ class Abbreviation_dictionary(object):
 	def generate_legend(self):
 		"""
 		Generate a unicode legend to print before the text. Right now
-		it's brittle because it assumes unicode renderer."""
+		it's brittle because it assumes unicode renderer.
+		"""
 		legend = ""
 		for key in self.lookup_table.keys():
-			legend += "{}: '{}'\n".format(self.lookup_table[key]['uni_rep'], self.lookup_table[key]['name'])
+			try:
+				legend += "{}: '{}'\n".format(self.lookup_table[key]['uni_rep'], self.lookup_table[key]['name'])
+			except KeyError:
+				legend += "{}: '{}'\n".format(self.lookup_table[key]['name'], self.lookup_table[key]['name'])
 		return legend
 
 def load_rules(filename):
+	"""
+	Build a config object from the provided file
+	"""
 	config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation(),allow_no_value=True)
 	config.read_file(filename)
 	return config
 
 def base_decode(text, abb_dict):
 	"""
-	Takes abbreviated text with unicode entities and renders them in ASCII 
+	Take abbreviated text with control entities and renders them in ASCII 
 	"""
 	working_text = list(text)
 	render = ""
@@ -133,10 +153,10 @@ def base_decode(text, abb_dict):
 		
 def uni_decode(text, abb_dict):
 	"""
-	Takes abbreviated text with unicode entities and renders them in unicode 
+	Take abbreviated text with control entities and renders them in unicode 
 	"""
 	working_text = list(text)
-	render = u""
+	render = ""
 	for char in working_text:
 		if 63743 > ord(char) >= 57344:
 			if abb_dict.uni_lookup(char):
@@ -153,6 +173,7 @@ if __name__ == "__main__":
 	import doctest
 	doctest.testmod()
 	
+	# Parse command line arguments
 	import argparse
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-g', '--generate', help="""
@@ -170,6 +191,7 @@ if __name__ == "__main__":
 					""", action="store_true") 
 	args = parser.parse_args()
 	
+	# Determine input method
 	if not sys.stdin.isatty():
 		text = sys.stdin.read().strip("\r\n")
 	elif args.infile:
@@ -178,13 +200,12 @@ if __name__ == "__main__":
 		text = " ".join(args.text)
 	else:
 		exit("No input received. Run 'python3 abba.py -h' for more information.")
-		
-	ruleset = open(args.ruleset)
 	
 	# Get a ruleset and use it to generate an abbreviation dictionary
 	if args.generate:
 		abb_set = generate_rules(text)
 	else:
+		ruleset = open(args.ruleset)
 		abb_set = load_rules(ruleset)
 	abba = Abbreviation_dictionary(abb_set)
 	
@@ -192,6 +213,7 @@ if __name__ == "__main__":
 	if args.legend:
 		legend = (abba.generate_legend())
 	
+	# Choose the rendering method	
 	if args.render == "unicode":
 		if args.legend: print(legend)
 		print(uni_decode(abba.abbreviate_text(text), abba))
--- regnet.py
+++ regnet.py
@@ -1,28 +1,15 @@
 import re, functools
 
-"""
-The processing language for describing matching patterns for abbreviations.
-Users can describe text with a series of tags, which are compiled into the appropriate regular expression. EG:
 
-'foo.initial' expands to a regular expression that matches all occurances of 'foo' at the beginning of a word.
-
-I need to be able to specify other classes in the regexp. For instance, 'before the end of the word or another suffix.' Not sure how to do that.
-
-A lot of these come to order of operation. So one way to deal with this is to process the tags in batches. For instance, as long as abbreviation characters are also word boundaries, then the only difference between suffix (ion) and final (s) is that s is rendered first.
-
-So in that case the compiler would return two objects - the compiled regexp, and the class that it falls under.
-
-Better, I think, to create a regnet object, which contains a compiled pattern and a precedence indicator.
-"""
 rules = [
-(re.compile("(?P<pat>\\w+)#pref"), "(?<=\\\\b)\g<pat>", 2), 
-(re.compile("(?P<pat>\\w+)#suf"), "\g<pat>(?=\\\\b)", 2),
+(re.compile("(?P<pat>\\w+)#pref"), "(?<=\\\\b)\g<pat>", 3), 
+(re.compile("(?P<pat>\\w+)#suf"), "\g<pat>(?=\\\\b)", 3),
 (re.compile("(?P<pat>\\w+)#init"), "(?<=\\\\b)\g<pat>", 0),
 (re.compile("(?P<pat>\\w+)#final"), "\g<pat>(?=\\\\b)", 0),
 (re.compile("(?P<pat>\\w+)#iso"), "(?<=\\\\b)\g<pat>(?=\\\\b)", 0),    
 (re.compile("(?P<pat>\\w+)#word"), "(?<=\\\\b)\g<pat>", 1),
-(re.compile("(?P<pat>^\\w\\w\\w$)"), "\g<pat>", 2),
-(re.compile("(?P<pat>^\\w\\w$)"), "\g<pat>", 3),
+(re.compile("(?P<pat>^\\w\\w\\w$)"), "\g<pat>", 3),
+(re.compile("(?P<pat>^\\w\\w$)"), "\g<pat>", 4),
 (re.compile("(?P<pat>^\\w$)"), "\g<pat>", -1),
 (re.compile("(?P<pat>\\w+)#vow"), "(?<=[aeiouy])\g<pat>", 2),
 (re.compile("(?P<pat>\\w+)#hard"), "\g<pat>", 0)
@@ -41,8 +28,6 @@ class Regnet(object):
 		And produces: 
 		a compiled regexp for "(?<=\\\\b)foo" and
 		prec = 0
-		
-		So actually, regnet rules are exclusive. Every pattern can only have one tag on it.
 		
 		"""
 		
